90abb7eb0e3c3116f7c206ac17a1d3930bff656c
yponion <yp071704@naver.com>
2024-08-27 23:15:56 +0900

    ✨feat: learn-typescript clone coding init

---
+++ why-ts/app.js
@@ -0,0 +1,53 @@
+// api url
+var url = "https://jsonplaceholder.typicode.com/users/1";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address.city;
+});
+
+function startApp() {
+  fetchUser()
+    .then(function (response) {
+      // console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerHTML = user.name;
+      email.innerHTML = user.email;
+      // address.innerHTML = user[0].address;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
---
+++ why-ts/sample.js
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// var result = sum(10, "20");
+
+// result.toLocaleString();
+
+// @ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+
+// sum(10, "20");
